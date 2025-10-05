779f2b330827627bc269d9e4fbf1f8f6fea27dfa
ThePankrat <ipankrat94@gmail.com>
2025-10-05 11:48:57 +0300

    fix: Add hello world message

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+print("Hello World") # <-- New line
 
 import datetime
 
 print("Welcome to Git Assignment")
+print(f"The current date and time is: {datetime.datetime.now()}")
 print("My name is [Your Name]")
-
